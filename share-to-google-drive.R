db79ae6b570aa08d89422d1458940e635a9c62a5
David Keyes <david@rfortherestofus.com>
2021-06-04 10:30:33 -0700

    Final version

--- share-to-google-drive.R
+++
@@ -1,9 +0,0 @@
-
-library(googledrive)
-library(rmarkdown)
-
-render(input = "slides.Rmd")
-
-drive_put("slides.html",
-          path = "https://drive.google.com/drive/u/1/folders/1ygw3-_I1zZlot6W2hNkOkrko6U2g3_HL",
-          name = "slides.html")
